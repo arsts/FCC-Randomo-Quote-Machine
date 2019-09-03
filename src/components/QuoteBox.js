6eefda7be2775d1507893d40cef9ee5fc108fd76
arsts <arstsu@gmail.com>
2019-09-03 18:28:38 +0300

    added quotebox component and quotes files

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class QuoteBox extends Component {
+  render() {
+    return (
+      <div>
+        <p>Quote: alalal!</p>
+        <i>Цитаты великих людей</i>
+      </div>
+    );
+  }
+}
+
+export default QuoteBox;
+
